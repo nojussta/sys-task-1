5523d459646e410ec2757983198a8538d459bb02
nojussta <nojus.stasiunas2@gmail.com>
2023-06-08 19:21:55 +0300

    add/check-if-dir

--- main.py
+++ main.py
@@ -11,5 +11,13 @@ def checkArgs():
     if len(sys.argv) < 2:
         print("Reikia bent vieno argumento")
         sys.exit(1)
+    else:
+        for i in range(1, len(sys.argv)):
+            if (os.path.isdir(sys.argv[i])):
+                print("Tai yra direktorija")
+                print(sys.argv[i])
+            else:
+                print("Tai nera direktorija, CLI priima tik direktorijas")
+
 
 main()
